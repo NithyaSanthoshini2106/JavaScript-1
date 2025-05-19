d9c60a7fcfe5f273b77c64af87274f5828621eb1
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-05-19 16:16:51 +0530

    ok

---
+++ Events-js/DOM1/data.js
@@ -0,0 +1,5 @@
+let tag_ref=document.getElementsByTagName('abc')
+
+console.log(tag_ref)
+
+tag_ref.innerHTML = "GM"
---
+++ Events-js/event-assignment/data.js
@@ -0,0 +1,6 @@
+function change_bin_color(){
+
+
+    alert('Test case 123')
+    document.getElementById('abc')
+}
